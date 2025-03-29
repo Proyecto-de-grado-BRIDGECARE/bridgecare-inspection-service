9ca489b4e0884476657af58cc63d6c58bedc2d6a
Juanba2002 <bajuan00@gmail.com>
2025-03-29 18:21:44 -0500

    Correcion InspeccionController

--- src/main/java/com/bridgecare/inspection/controllers/InspeccionController.java
+++ src/main/java/com/bridgecare/inspection/controllers/InspeccionController.java
@@ -2,6 +2,7 @@ package com.bridgecare.inspection.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,8 @@ public class InspeccionController {
     private InspeccionService inspeccionService;
 
     @PostMapping("/add")
-    public ResponseEntity<String> createInspeccion(@RequestBody InspeccionDTO request) {
-        Long inspeccionId = inspeccionService.saveInspection(request);
-        return ResponseEntity.ok("Inspeccion created with ID: " + inspeccionId);
+    public ResponseEntity<String> addInventario(@RequestBody InspeccionDTO request, Authentication authentication) {
+        Long inspeccionId = inspeccionService.saveInspeccion(request, authentication);
+        return ResponseEntity.ok("Inventario created with ID: " + inspeccionId);
     }
 }
